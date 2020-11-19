a2495262d138e729075c0cd38fbdefcd1dbfcfcb
Vitaliy Danilov <vitaliy931@gmail.com>
2020-11-19 12:36:56 +0200

    net 5.0

--- AbnormalDisconnectTest/Program.cs
+++ AbnormalDisconnectTest/Program.cs
@@ -2,36 +2,33 @@
 using System.Threading;
 using DanilovSoft.MicroORM;
 
-namespace AbnormalDisconnectTest
+class Program
 {
-    class Program
+    private static readonly SqlORM _orm = new SqlORM("Server=mt.tiraz.net; Port=5432; User Id=postgres; Password=test; Database=postgres; " +
+        "Pooling=true; MinPoolSize=1; MaxPoolSize=100", Npgsql.NpgsqlFactory.Instance);
+
+    static void Main()
     {
-        private static readonly SqlORM _orm = new SqlORM("Server=mt.tiraz.net; Port=5432; User Id=postgres; Password=test; Database=postgres; " +
-            "Pooling=true; MinPoolSize=1; MaxPoolSize=100", Npgsql.NpgsqlFactory.Instance);
+        Console.WriteLine("Этот тест сработает только когда БД находится в интернете.");
 
-        static void Main()
+        // Подготовим одно живое соединение.
+        _orm.Sql("SELECT 1").Execute();
+
+        try
         {
-            Console.WriteLine("Этот тест сработает только когда БД находится в интернете.");
+            var task = _orm.Sql("SELECT pg_sleep(60)")
+                .Timeout(timeoutSec: 20) // таймаут запроса
+                .ToAsync()
+                .Execute();
 
-            // Подготовим одно живое соединение.
-            _orm.Sql("SELECT 1").Execute();
+            Console.WriteLine("Пора выдернуть Ethernet кабель. На это есть 20 секунд");
 
-            try
-            {
-                var task = _orm.Sql("SELECT pg_sleep(60)")
-                    .Timeout(timeoutSec: 20) // таймаут запроса
-                    .ToAsync()
-                    .Execute();
-
-                Console.WriteLine("Пора выдернуть Ethernet кабель. На это есть 20 секунд");
-
-                task.GetAwaiter().GetResult();
-            }
-            catch (SqlQueryTimeoutException ex)
-            {
-                Console.WriteLine("Успешно сработал таймаут");
-            }
-            Thread.Sleep(-1);
+            task.GetAwaiter().GetResult();
         }
+        catch (SqlQueryTimeoutException ex)
+        {
+            Console.WriteLine("Успешно сработал таймаут");
+        }
+        Thread.Sleep(-1);
     }
 }
